afca123963fcdeff3b2628b62c3f024d1da18a4f
Nic Schumann <nicolas_schumann@brown.edu>
2014-12-01 11:25:27 -0500

    minor amendment to redirection logic.

--- wp-content/themes/coco/_partials/small-login.php
+++ wp-content/themes/coco/_partials/small-login.php
@@ -1,4 +1,6 @@
-<form name="small-login" id="small-login" class="col-sm-4 right" action="http://localhost:8888/coco/wp-login.php" method="post">
+<form name="small-login" id="small-login" class="col-sm-4 right" action="http://localhost:8888/coco/wp-login.php" method="post">  
+  <?php global $wp; $url = home_url(add_query_arg(array(),$wp->request)); ?>
+
   <p class="login-username">
     <input type="text" name="log" id="user_login" class="input" value="" placeholder="USERNAME" size="20">
   </p>
@@ -7,7 +9,7 @@
   </p>
   <p class="login-submit">
     <input type="submit" name="wp-submit" id="wp-submit" class="button-primary" value="Log In">
-    <input type="hidden" name="redirect_to" value="http://localhost:8888/coco/"><!-- BAD -->
+    <input type="hidden" name="redirect_to" value="<?php echo $url; ?>">
   </p>
   
 </form>
