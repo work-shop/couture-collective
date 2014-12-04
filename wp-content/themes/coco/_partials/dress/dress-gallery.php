25bb4a85ec3a32ef26f042d3adc2ee758b4fdb52
Nic Schumann <nicolas_schumann@brown.edu>
2014-12-04 08:52:27 -0500

    block buildout of gallery

--- wp-content/themes/coco/_partials/dress/dress-gallery.php
+++ wp-content/themes/coco/_partials/dress/dress-gallery.php
@@ -1,3 +1,27 @@
 <?php
-	$gallery = get_field('dress_images', get_the_ID() );
+	$images = get_field('dress_images', get_the_ID() );
+
+	if ( $images ) {
+?>
+
+	<div class="row">
+		<div class="col-sm-1">
+			<div class="row">
+			<!-- gallery grid -->
+
+			</div>
+		</div>
+		<div class="col-sm-11">
+			<!-- gallery image -->
+			
+		</div>
+	</div>
+
+<?php
+	} else {
+?>
+
+
+<?php
+	}
 ?>
