bc1cbe933fbd2160610137ac3c5aba7e6a07e7c2
Nic Schumann <nicolas_schumann@brown.edu>
2014-12-05 11:34:55 -0500

    naming issue in share

--- wp-content/themes/coco/_partials/dress/dress-share-unsold.php
+++ wp-content/themes/coco/_partials/dress/dress-share-unsold.php
@@ -23,7 +23,7 @@ if ( $tv ) { ?>
 	<form class="cart" method="post" enctype='multipart/form-data'>
 	 	<?php do_action( 'woocommerce_before_add_to_cart_button' ); ?>
 
-	 	<input type="hidden" name="add-to-cart" value="<?php echo esc_attr( $product->id ); ?>" />
+	 	<input type="hidden" name="add-to-cart" value="<?php echo esc_attr( $share->id ); ?>" />
 	 	<!-- <input type="hidden" name="redirect_to" value="<?php echo $url; ?>"> -->
 
 	 	<button type="submit" class="single_add_to_cart_button button alt">PURCHASE</button>
