b868d187caea0056c72867ded0548253f05a3d5c
Nic Schumann <nicolas_schumann@brown.edu>
2014-12-07 10:00:20 -0500

    added markup for filtering primitives; static

--- wp-content/themes/coco/page-look-book.php
+++ wp-content/themes/coco/page-look-book.php
@@ -9,14 +9,25 @@
 	
 		<div class="products">
 			<div class="container">
+				<?php  if ( is_user_logged_in() ) : ?>
+				<div class='row filter'>
+					<div class='col-sm-6'></div>
+					<div class='col-sm-2'><a href="#">Owned</a></div>
+					<div class='col-sm-2'><a href="#">Shares Available</a></div>
+					<div class='col-sm-2'><a href="#">Purchase Available</a></div>
+				</div>
+				<?php endif; ?>
+				
 				<div class="row">
-			<?php
+				<?php
 				$args = array(
 					'post_type' => 'dress',
-					'posts_per_page' => 12,
+					'posts_per_page' => -1,
 					'post__not_in' => array(45)
 				);
+
 				$GLOBALS['LOOP'] = new WP_Query( $args );
+				$GLOBALS['USER'] = wp_get_current_user();
 
 				if ( $GLOBALS['LOOP']->have_posts() ) {
 
@@ -26,7 +37,11 @@
 
 				} else { 
 					echo '<h1>There aren\'t any dresses right now!</h1>';
-				} ?>
+				} 
+
+
+
+				?>
 				</div>
 			</div>
 		</div>
