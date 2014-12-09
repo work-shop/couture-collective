68e46e8544ac3174a109922bfd6d430978500a83
Nic Schumann <nicolas_schumann@brown.edu>
2014-12-09 15:21:48 -0500

    recoverey...

--- wp-content/themes/coco/page-cancel-reservation.php
+++ wp-content/themes/coco/page-cancel-reservation.php
@@ -3,5 +3,51 @@
 	Template Name: Cancel-Reservation
 */
 ?>
-<?php get_template_part('_partials/placeholder/placeholder', 'forward' ); ?>
+<?php
+// if the required post-variables are not set, redirect to a 304-page.
 
+if ( isset( $_POST['referring-page']) && isset($_POST['user-id']) && isset($_POST['booking-id']) ) {
+	if ( get_current_user_id() == ( $id = $_POST['user-id'] ) ) {
+		$val = find_requested_booking( WC_Bookings_Controller::get_bookings_for_user( $id ), $_POST['booking-id'] );
+
+		if ( $val ) {
+			$or = $val->get_order();
+			$items = $or->get_items();
+
+			if ( 1 == count( $items ) ) {
+				$or_succ = $or->update_status('cancelled');
+			} else {
+				$or_succ = true;
+			}
+
+			// why isn't the response switch appropriate here?
+
+			$bk_succ = $val->update_status('cancelled');
+
+			if ( $or_succ && $bk_succ ) {
+				wc_add_notice( 'your booking was successfully cancelled.','success' );
+				wp_redirect( $_POST['referring-page']);
+				exit;
+			} else {
+				wc_add_notice( '(1) we couldn\'t cancel your booking!','notice' );
+				wp_redirect( $_POST['referring-page'] );
+				exit;
+			}
+		} else {
+			// internal cancellation error.
+			wc_add_notice( '(2) we couldn\'t cancel your booking!','notice' );
+			wp_redirect( $_POST['referring-page'] );
+			exit;
+		}
+	} else {
+		// permission denied -- user session does not match authenticated user
+		// add and check a nonce, too.
+		wp_redirect( home_url() );
+		exit;
+	}
+} else {
+	wp_redirect( home_url() );
+	exit;
+}
+
+?>
