a07e1365b3781a86f95ded53d34dbc20cbe74d65
Greg Nemes <mail@gregnemes.com>
2014-12-09 21:43:19 -0500

    finalization of front end alpha features, v2

--- wp-content/themes/coco/_partials/dress/dress-gallery.php
+++ wp-content/themes/coco/_partials/dress/dress-gallery.php
@@ -5,12 +5,12 @@
 ?>
 
 	<div class="row">
-		<div class="col-sm-3">
+		<div class="col-sm-3 col-xs-2">
 			<div class="row">
 
 			</div>
 		</div>
-		<div class="col-sm-9">
+		<div class="col-sm-9 col-xs-8">
 			<?php 
 			if ( has_post_thumbnail() ) {
 				the_post_thumbnail('gallery');
