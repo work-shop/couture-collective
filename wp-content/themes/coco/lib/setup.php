23c92a0bd0153962563b96c769639d4bbfaf46cf
Nic Schumann <nicolas_schumann@brown.edu>
2014-12-11 10:50:24 -0500

    lib rebuild

--- wp-content/themes/coco/lib/setup.php
+++ wp-content/themes/coco/lib/setup.php
@@ -11,7 +11,7 @@ function theme_scripts() {
     wp_register_script( 'bootstrap', get_template_directory_uri() . '/_/js/bootstrap.js');
     wp_register_script( 'flexslider', get_template_directory_uri() . '/_/js/flexslider.js');
     wp_register_script( 'functions', get_template_directory_uri() . '/_/js/functions.js');
-    wp_register_script( 'ajax', get_template_directory_uri() . '/_/js/ajax.js' );
+    wp_register_script( 'cc-ajax-make-reservation', get_template_directory_uri() . '/_/js/ajax/make-reservation.js' );
 
     wp_enqueue_script( 'jquery' );
     wp_enqueue_script( 'less' );    
