cadc17041dc471668d199bb0b97b0f2cfd35dee0
Greg Nemes <mail@gregnemes.com>
2014-12-16 10:24:16 -0500

    site usage terms page

--- wp-content/themes/coco/page-site-usage-terms.php
+++ wp-content/themes/coco/page-site-usage-terms.php
@@ -13,8 +13,6 @@
 				<div class="col-sm-10 col-sm-offset-1">
 				
 					<h1 class="serif centered m"><?php the_title(); ?></h1>
-					
-					<h3 class="centered">All memberships are at the invitation of Couture Collective (Co.Co.) and subject to the terms and conditions of the “Membership Agreement” as set out below:</h3>
 									
 				</div>
 
@@ -29,156 +27,150 @@
 			<div class="row">
 				
 				<div class="col-sm-10 col-sm-offset-1">
-					<p class="h3 centered m25">
-					Membership is subject to approval upon application to Couture Collective. All applications must be accompanied by a valid credit card authorization form. Upon approval, a yearly membership fee of $250 will be charged and the member will be able to access the current Co.Co. Collection and add items to her virtual closet by purchasing a share in that item.
+					<h5>Site License and Access</h5>
+				
+					<p class="h3 m">
+					By accessing this Site, you agree that your access to, and use of the Site will be subject to the
+conditions set forth in this legal notice and all applicable laws. If you do not agree and accept, without
+limitation or qualification, these terms, please exit the Site.
 					</p>
-				</div>
-				
-				<div class="col-sm-5 col-sm-offset-1 col-md-4 col-md-offset-1 descriptor">
-				
-					<div class="descriptor-text centered">
-						<h5 class="m">Termination by Member</h5>
-
-						<p class="h3">
-
-						 A member may terminate membership at any time. If you wish to de-activate your account, please contact info@couturecollective.club. 
-						</p>
-					</div>
 					
-				</div>	
-				
-				<div class="col-sm-5 col-md-4 col-md-offset-2 descriptor">
-				
-					<div class="descriptor-text centered">
-						<h5 class="m">Termination by Co.Co.</h5>
-						<p class="h3">
-						A violation of the terms and conditions of this agreement may result in a suspension or termination of your membership privileges. 
-						</p>
-					</div>
+					<p class="h3 m">
+This Site and all of its content, including, but not limited to, all text, graphics, logos, button icons,
+images, software, and the presentation contained herein, belongs to Couture Collective, LLC “
+Co.Co.” or its third party licensors. The design and layout of this Site (the look and feel of the Site) is
+the exclusive property of Couture Collective, LLC and may not be used, copied, distributed or
+displayed in any way. All trademarks, logos, or service marks, whether registered or unregistered, are
+proprietary to Couture Collective, LLC or its third party licensors. This Site may contain proprietary
+notices and copyright information, the terms of which must be observed and followed.
+					</p>					
 					
-				</div>							
-				
-			</div>
-								
-		</div>			
-	</section>	
-
-	<section id="terms-share" class="block m3">	
-		<div class="container">
-			
-			<div class="row">
-			
-				<div class="col-sm-12">
-					<h4 class=" bordered serif centered m25">Share Purchase</h4>
-				</div>
-				
-				<div class="col-sm-10 col-sm-offset-1">
-					<p class="h3 centered">
-					The share purchase for an Item will be 20% of the retail price of the Item, plus applicable sales tax. Purchase of a share in any Co.Co. item entitles the owners to five reserved wearings. No more than five (5) shares will be sold in any single item and access to an item’s calendar will be first-come- first-served for reservation priority. Every effort will be made to minimize “down time” for your purchased item, but proper inspection, cleaning and care between flights along with delivery windows will require between 3-5 days between reservations.
+					<p class="h3 m">
+					Couture Collective, LLC Inc. grants you a limited license to make personal use of the Site. Except as
+noted, you are not conveyed any right or license by implication, estoppel, or otherwise, to use,
+reproduce, transmit, perform, publish, license, modify, rewrite, create derivative works from, transfer or
+sell any of the content or information contained within this Site, including but not limited to, any of
+Couture Collective, LLC’ intellectual property.
 					</p>
-				</div>					
-				
-			</div>
-								
-		</div>			
-	</section>	
-
-	<section id="terms-fees" class="block m3">	
-		<div class="container">
-			
-			<div class="row">
-			
-				<div class="col-sm-12">
-					<h4 class=" bordered serif centered m25">Late Fees</h4>
-				</div>
-				
-				<div class="col-sm-10 col-sm-offset-1">
-					<p class="h3 centered">
-					Items that are not returned on time interfere with other owners’ rights and privileges and cause problems with the Co.Co. membership model. All items are expected to be ready for local pick-up 48 hours after delivery unless it is reserved for consecutive flights. Items that are not ready for pick-up or returned within two business days of delivery will be considered Late and will incur a Late Fee of $100 per day until the item is returned. Items kept for twenty (14) days beyond the date of expected return will be considered a Non-Return and the member’s account will be charged the full retail price of the garment minus her original share purchase price, in addition to the already incurred Late Fees. Late Fees will not be refunded in the case of a Non-Return, but retained in order to expedite a replacement for the other owner members.
-					</p>
-				</div>					
-				
-			</div>
-								
-		</div>			
-	</section>	
-
-	<section id="terms-delivery" class="block m3">	
-		<div class="container">
-			
-			<div class="row">
-			
-				<div class="col-sm-12">
-					<h4 class=" bordered serif centered m25">Delivery and Cleaning</h4>
-				</div>
-				
-				<div class="col-sm-10 col-sm-offset-1">
-					<p class="h3 centered">
-Members should not undertake to clean any garment themselves. All members agree to pay any additional cleaning costs associated with their wearing. While a standard cleaning cost will be displayed along with each item, any additional care deemed necessary such as spot cleaning or minor garment repair is at the discretion of Hallak Couture Cleaners, where the items will be inspected and stored. Local Delivery and Pickup are free as indicated in each item’s “more information” section. Fed-ex two-day shipping charges are charged to the member and will arrive with return packaging and instructions.
-					</p>
-				</div>
-			
-			</div>
-								
-		</div>			
-	</section>	
-
-	<section id="terms-loss" class="block m3">	
-		<div class="container">
-			
-			<div class="row">
-			
-				<div class="col-sm-12">
-					<h4 class=" bordered serif centered m25">Loss or Irreparable Damage to Property</h4>
-				</div>
-				
-				<div class="col-sm-5 col-sm-offset-1 col-md-4 col-md-offset-1 descriptor">
-				
-					<div class="descriptor-text centered">
-						<h5 class="m">Minor Repairs</h5>
-						<p class="h3">
-Upon purchasing a share in a Co.Co. item, you agree to take the utmost care with the garment, as you would your own. The nature of high couture fashion requires that dressing, undressing and storage while in your possession meet a certain standard. Please review our Care and Courtesy section below. Minor issues such as treatable stains, tiny rips or stuck zippers will be repaired and incur a repair charge to the member. You are responsible for any loss, damage, destruction or theft of the garment.  
-						</p>
-					</div>
 					
-				</div>	
+					<p class="h3 m">
+					Your limited license to use does not include, without limitation: (a) any resale or commercial use of the
+Site or content therein; (b) the collection and use of any product listings or descriptions; (c) making
+derivative use of the Site and its contents; and (d) use of any data mining, robots, or similar data
+gathering and extraction methods. You may not use, frame, or utilize framing techniques to enclose
+any Couture Collective, LLC trademark, logo or other proprietary information (including the images
+found at this Site, the content of any text or the layout/design of any page or form contained on a
+page) without Couture Collective, LLC’s express written consent. Further, you may not use any meta
+tags or any other "hidden text" utilizing any Couture Collective, LLC name, trademarks, or product
+names without Couture Collective, LLC’ express written consent.
+					</p>					
 				
-				<div class="col-sm-5 col-md-4 col-md-offset-2 descriptor">
-				
-					<div class="descriptor-text centered">
-						<h5 class="m">Irreparable Damage</h5>
-						<p class="h3">
-At the time of your purchase of a share in an Item, a member authorizes Co.Co. to charge her payment card for an amount equal to 110% of the original retail value of the Item (when new) plus applicable sales taxes, in case of irreparable damage or loss. Co.Co. must be notified immediately in the case of major damage or loss to avoid Late Fees which are not refundable.						</p>
-					</div>
 					
-				</div>							
+					<p class="h3 m">
+					No images on this site may be used without first seeking Couture Collective, LLC’ express permission.
+To request permission to use Couture Collective, LLC’ intellectual property, email Couture Collective,
+LLC at info@couturecollective.club. Couture Collective, LLC reserves the right to refuse any permission
+request for any reason.
+					</p>
+									
+					<p class="h3 m25">
+Any unauthorized use of this Site will immediately terminate the limited license and/or rights granted
+herein. Unauthorized uses of Couture Collective, LLC’s intellectual property may violate applicable laws
+including copyright laws, trademark laws (including trade dress), and communications regulations and
+statutes. All violators will be prosecuted to the fullest extent of the law, including the seeking of
+criminal prosecution when warranted.
+					</p>
+					
+					<h5>Changes/Accuracy of Information</h5>
+					
+					<p class="h3 m">
+Information may be changed or updated without notice. Couture Collective, LLC may also make
+improvements and/or changes in the products and/or programs described in this information at any
+time without notice.
+					</p>										
+									
+					<p class="h3 m25">
+Couture Collective, LLC assumes no responsibility regarding the accuracy of the information that is
+provided by this Site and use of such information is at the recipient’s own risk. Information on this Site
+may contain technical inaccuracies or typographical errors and may not be complete. Please note that
+such errors or inaccuracies or omissions may relate to the pricing of items available for purchase on
+this Site. Couture Collective, LLC reserves the right to cancel or refuse to accept any order placed for
+any reason. Prices and availability are subject to change without notice. Couture Collective, LLC
+apologizes for any inconvenience. Reference should be made to the program documents for each item
+for precise terms and conditions.
+					</p>	
+					
+					<h5>Colors</h5>
+					
+					<p class="h3 m25">
+Couture Collective, LLC has made every effort to ensure that the colors of the images on its Site,
+including those of items for sale, are depicted as accurately as possible. However, as monitors and
+other user equipment vary, Couture Collective, LLC cannot guarantee that your monitor’s display of
+color will be accurate.
+					</p>										
+					
+					<h5>Global Availability</h5>
+					
+					<p class="h3 m">
+Information Couture Collective, LLC publishes on the World Wide Web may contain references or cross
+references to Couture Collective, LLC’s products, programs and services that are not announced or
+available in your country. Such references do not imply that Couture Collective, LLC intends to
+announce such products, programs or services in your country.
+					</p>										
+									
+					<p class="h3 m25">
+Couture Collective, LLC’ obligations with respect to its products and services are governed solely by
+the agreements under which they are provided, If you obtain a product or service from Couture
+Collective, LLC off this Site that is provided without an agreement, that product or service is provided
+"AS-IS" with no warranties whatsoever, express or implied, and your use of that product or service is at
+your own risk.
+					</p>						
+					
+					<h5>Links</h5>
+					
+					<p class="h3 m25">
+Couture Collective, LLC makes no representations whatsoever about any other site which you may
+access through this one. When you access a non-Couture Collective, LLC Web site, even one that may
+contain the Couture Collective, LLC’ logo, please understand that it is independent from Couture
+Collective, LLC, and that Couture Collective, LLC has no control over the content on that Web site. In
+addition, a link to a non-Couture Collective, LLC Web site does not mean that Couture Collective, LLC
+endorses or accepts any responsibility for the content, privacy policies, security or the use of such a
+Web site. It is up to you to take precautions to ensure that your use is free of such items as viruses,
+worms, Trojan horses and other items of a destructive nature.
+					</p>						
+					
+						<h5>Disclaimer of Warranties</h5>
+					
+					<p class="h3 m25">
+All information is provided by Couture Collective, LLC on an "AS-IS" basis only. Couture Collective, LLC provides no representations and warranties, express or implied, including the implied warranties of fitness for a particular purpose, merchantability and non-infringement.
+					</p>					
+					
+					
+					<h5>Exclusion of liability</h5>
+					
+					<p class="h3 m25">
+Your use of the site and any of its content or features is at your own risk. In no event will Couture Collective, LLC nor any of its subsidiaries, affiliates, officers, directors, agents, contractors, representatives, employees, successors, or assigns, or any other party involved in creating, producing, or delivering this site, be liable to any party for any direct, indirect, punitive, incidental, special or other consequential damages for any use of this web site or content, or on any other hyper-linked web site, including, without limitation, any lost profits, business interruption, loss of programs or other data on your information handling systems or otherwise, even if we are expressly advised of the possibility of such damages. Because some states do not allow the limitation or exclusion of liability for consequential or incidental damages, the above limitation may not apply to you.
+					</p>		
+					
+					<h5>Law and Jurisdiction</h5>
+					
+					<p class="h3 m25">
+This Legal Statement and your use of the Site are governed by the laws of the State of New York without regard to its choice of law provisions. The courts of general jurisdiction located within New York shall have exclusive jurisdiction over any and all disputes arising out of, relating to, or concerning this Legal Statement and/or the Site or in which this Legal Statement and/or the Site are a material fact.
+					</p>		
+					
+					<h5>General</h5>
+					
+					<p class="h3 m25">
+This Legal Statement may not be modified except in writing. If any of these terms shall be deemed unlawful, void, or for any reason unenforceable, that provision shall be severed from these terms and shall not affect the validity and enforceability of the remaining provisions.
+					</p>															
+																												
+				</div>										
 				
 			</div>
 								
 		</div>			
 	</section>	
 
-	<section id="terms-care" class="block m3">	
-		<div class="container">
-			
-			<div class="row">
-			
-				<div class="col-sm-12">
-					<h4 class=" bordered serif centered m25">Care and Courtesy</h4>
-				</div>
-				
-				<div class="col-sm-10 col-sm-offset-1">
-					<p class="h3 centered m25">
-						Please refrain from spot cleaning your garment. Please do not apply an iron directly to the garment. Please apply perfume before putting on your garment. Please use paper shields when sliding your garment over make-up. Please refrain from sitting directly on bamboo or concrete seats as they can snag delicate fabrics (requesting an extra napkin usually does the trick).
-						Please hang up your garment after wearing. 
-					</p>
-				</div>				
-				
-			</div>
-								
-		</div>			
-	</section>	
-	
 </div>	
 
 <?php get_footer(); ?>
