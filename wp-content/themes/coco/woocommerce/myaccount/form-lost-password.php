96c081fefba6d75b0e7ba627d1d6e0cfc0630066
Nic Schumann <nicolas_schumann@brown.edu>
2014-12-16 15:37:51 -0500

    changed structure in the my-account templete hierarchy

--- wp-content/themes/coco/woocommerce/myaccount/form-lost-password.php
+++ wp-content/themes/coco/woocommerce/myaccount/form-lost-password.php
@@ -12,37 +12,67 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 ?>
 
+<section id="account-intro" class="block m3">
+    
+    <div class="container">
+        <div class="row">
+        
+            <hr class="page-header-rule" />
+        
+            <div class="col-sm-12">
+                <h1 class="centered">Lost Password</h1>
+            </div>
+            
+        </div>
+        
+    </div>  
+    
+</section>  
+
+
 <?php wc_print_notices(); ?>
 
-<form method="post" class="lost_reset_password">
+<section id="account-information" class="block m3">
+        
+        <div class="container">
+            <div class="row">   
+                
+                <div class="col-sm-12">
 
-	<?php if( 'lost_password' == $args['form'] ) : ?>
+                    <form method="post" class="lost_reset_password">
 
-        <p><?php echo apply_filters( 'woocommerce_lost_password_message', __( 'Lost your password? Please enter your username or email address. You will receive a link to create a new password via email.', 'woocommerce' ) ); ?></p>
+                    	<?php if( 'lost_password' == $args['form'] ) : ?>
 
-        <p class="form-row form-row-first"><label for="user_login"><?php _e( 'Username or email', 'woocommerce' ); ?></label> <input class="input-text" type="text" name="user_login" id="user_login" /></p>
+                            <p><?php echo apply_filters( 'woocommerce_lost_password_message', __( 'Lost your password? Please enter your username or email address. You will receive a link to create a new password via email.', 'woocommerce' ) ); ?></p>
 
-	<?php else : ?>
+                            <p class="form-row form-row-first"><label for="user_login"><?php _e( 'Username or email', 'woocommerce' ); ?></label> <input class="input-text" type="text" name="user_login" id="user_login" /></p>
 
-        <p><?php echo apply_filters( 'woocommerce_reset_password_message', __( 'Enter a new password below.', 'woocommerce') ); ?></p>
+                    	<?php else : ?>
 
-        <p class="form-row form-row-first">
-            <label for="password_1"><?php _e( 'New password', 'woocommerce' ); ?> <span class="required">*</span></label>
-            <input type="password" class="input-text" name="password_1" id="password_1" />
-        </p>
-        <p class="form-row form-row-last">
-            <label for="password_2"><?php _e( 'Re-enter new password', 'woocommerce' ); ?> <span class="required">*</span></label>
-            <input type="password" class="input-text" name="password_2" id="password_2" />
-        </p>
+                            <p><?php echo apply_filters( 'woocommerce_reset_password_message', __( 'Enter a new password below.', 'woocommerce') ); ?></p>
 
-        <input type="hidden" name="reset_key" value="<?php echo isset( $args['key'] ) ? $args['key'] : ''; ?>" />
-        <input type="hidden" name="reset_login" value="<?php echo isset( $args['login'] ) ? $args['login'] : ''; ?>" />
+                            <p class="form-row form-row-first">
+                                <label for="password_1"><?php _e( 'New password', 'woocommerce' ); ?> <span class="required">*</span></label>
+                                <input type="password" class="input-text" name="password_1" id="password_1" />
+                            </p>
+                            <p class="form-row form-row-last">
+                                <label for="password_2"><?php _e( 'Re-enter new password', 'woocommerce' ); ?> <span class="required">*</span></label>
+                                <input type="password" class="input-text" name="password_2" id="password_2" />
+                            </p>
 
-    <?php endif; ?>
+                            <input type="hidden" name="reset_key" value="<?php echo isset( $args['key'] ) ? $args['key'] : ''; ?>" />
+                            <input type="hidden" name="reset_login" value="<?php echo isset( $args['login'] ) ? $args['login'] : ''; ?>" />
 
-    <div class="clear"></div>
+                        <?php endif; ?>
 
-    <p class="form-row"><input type="submit" class="button" name="wc_reset_password" value="<?php echo 'lost_password' == $args['form'] ? __( 'Reset Password', 'woocommerce' ) : __( 'Save', 'woocommerce' ); ?>" /></p>
-	<?php wp_nonce_field( $args['form'] ); ?>
+                        <div class="clear"></div>
 
-</form>
+                        <p class="form-row"><input type="submit" class="button" name="wc_reset_password" value="<?php echo 'lost_password' == $args['form'] ? __( 'Reset Password', 'woocommerce' ) : __( 'Save', 'woocommerce' ); ?>" /></p>
+                    	<?php wp_nonce_field( $args['form'] ); ?>
+
+                    </form>
+
+            </div>
+        </div>
+    </div>
+</section>
