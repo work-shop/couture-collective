0d3924ffa881e775eccc6620bf4429954310c9f9
Greg Nemes <mail@gregnemes.com>
2014-12-16 10:07:58 -0500

    fixed dress single size variable bug

--- wp-content/themes/coco/_partials/dress/dress-setup-postdata.php
+++ wp-content/themes/coco/_partials/dress/dress-setup-postdata.php
@@ -9,12 +9,11 @@ $GLOBALS['CC_POST_DATA']['title'] = apply_filters( 'the_title', get_the_title() 
 $GLOBALS['CC_POST_DATA']['description'] = get_field( 'dress_description', $id );
 $GLOBALS['CC_POST_DATA']['designer'] = get_field( 'dress_designer', $id );
 $GLOBALS['CC_POST_DATA']['images'] = get_field('dress_images', $id);
+$GLOBALS['CC_POST_DATA']['size'] = get_field('dress_size', $id);
 
 
 
 if ( $GLOBALS['CC_POST_DATA']['logged_in'] ) {
-
-	$GLOBALS['CC_POST_DATA']['size'] = get_field('dress_size', $id);
 	$GLOBALS['CC_POST_DATA']['sale'] = new WC_Product( get_field('dress_sale_product_instance', $id )[0]->ID );
 	$GLOBALS['CC_POST_DATA']['share'] = new WC_Product( get_field('dress_share_product_instance', $id )[0]->ID );
 	$GLOBALS['CC_POST_DATA']['rental'] = new WC_Product_Booking( get_field('dress_rental_product_instance', $id)[0]->ID );
