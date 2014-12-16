0d2c16f6d0ed32644b24a54e8de2827375c67971
Nic Schumann <nicolas_schumann@brown.edu>
2014-12-16 15:03:34 -0500

    Restructured shortcode call

--- wp-content/themes/coco/page-my-account.php
+++ wp-content/themes/coco/page-my-account.php
@@ -29,12 +29,21 @@
 				</div>
 				
 				<div class="col-sm-12">
-					<?php echo do_shortcode('[woocommerce_my_account]') ?>
+					<?php
+					//$addr_type = "billing";
+					$customer_id = get_current_user_id();
+					// do_action('woocommerce_before_my_account'); // hooks my subscriptions right now
+					//wc_get_template( 'myaccount/form-edit-account.php', array('user' => wp_get_current_user() ) );
+					WC_Shortcode_My_Account::output( array() );
+					//WC_Shortcode_My_Account::output( array() );
+					// do_action('woocommerce_after_my_account'); // hooks my saved cards right now
+					?>
+					<?php //echo do_shortcode('[woocommerce_my_account]') ?>
 				</div>			
 				
-				<div class="col-sm-3">
+				<!-- <div class="col-sm-3">
 					<a class="h7 centered m2 bg-pink-darker display-block contact-link white uppercase" href="<?php wc_customer_edit_account_url(); ?>">Edit Profile</a>	
-				</div>
+				</div> -->
 				
 			</div>
 		</div>
