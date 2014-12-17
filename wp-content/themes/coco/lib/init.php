239869f8af4e070ea8d70574052328cc3d1c6292
Nic Schumann <nicolas_schumann@brown.edu>
2014-12-17 10:52:10 -0500

    set init hooks

--- wp-content/themes/coco/lib/init.php
+++ wp-content/themes/coco/lib/init.php
@@ -2,8 +2,5 @@
 	require_once('defs.php');
 	require_once('lib.php');
 	require_once('setup.php');
-	require_once('post-types.php');
-	require_once('actions.php');
-	// require_once('ajax/init.php');
 	require_once('class/init.php');
 	
