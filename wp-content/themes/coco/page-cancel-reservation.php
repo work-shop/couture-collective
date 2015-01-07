31b1c4ed95a937647d79abd41aab440413f14644
Nic Schumann <nicolas_schumann@brown.edu>
2015-01-07 08:56:36 -0500

    updated cancel reservation logic, up to refund (needs testing)

--- wp-content/themes/coco/page-cancel-reservation.php
+++ wp-content/themes/coco/page-cancel-reservation.php
@@ -7,35 +7,72 @@
 // if the required post-variables are not set, redirect to a 304-page.
 
 if ( isset( $_POST['referring-page']) && isset($_POST['user-id']) && isset($_POST['booking-id']) ) {
-	if ( get_current_user_id() == ( $id = $_POST['user-id'] ) ) {
-		$val = find_requested_booking( WC_Bookings_Controller::get_bookings_for_user( $id ), $_POST['booking-id'] );
+	$user = wp_get_current_user();
+
+	if ( $user->ID == ( $id = $_POST['user-id'] ) ) {
+		$val = CC_Controller::validate_requested_booking( WC_Bookings_Controller::get_bookings_for_user( $id ), $_POST['booking-id'] );
 
 		if ( $val ) {
 			$or = $val->get_order();
-			$items = $or->get_items();
 
-			if ( 1 == count( $items ) ) {
-				$or_succ = $or->update_status('cancelled');
-			} else {
-				$or_succ = true;
+			if ( $or->get_user_id() != $user->ID ) {
+				wc_add_notice( '(ERR-3) The requesting user doesn\'t match the order owner','notice' );
+				wp_redirect( $_POST['referring-page'] );
+				exit;
 			}
 
-			// why isn't the response switch appropriate here?
+
+			$items = $or->get_items();
+			$item = CC_Controller::get_order_item_for_booking( $val, $or );
+
+			var_dump( $item );
+			exit;
+
+			$refund = wc_create_refund( array(
+				'amount' => CC_Controller::get_refund_amount( $item['id'], $item['set'] ),
+				'reason' => $user->display_name . ' ('. $user->user_email .') canceled the booking.',
+				'order_id' => $or->id,
+				'refund_id' => 0
+			));
 
 			$bk_succ = $val->update_status('cancelled');
 
-			if ( $or_succ && $bk_succ ) {
+			if ( $bk_succ && $refund ) {
+				// if we've reached this point, let's try and refund the thing v. the Stripe API:
+
+
+				if ( WC()->payment_gateways() ) {
+					$payment_gateways = WC()->payment_gateways->payment_gateways();
+				}
+
+				if ( isset( $payment_gateways[ $or->payment_method ] ) && $payment_gateways[ $or->payment_method ]->supports( 'refunds' ) ) {
+					$result = $payment_gateways[ $or->payment_method ]->process_refund( $order_id, $or->get_refund_amount(), $refund->get_refund_reason() );
+
+					if ( is_wp_error( $result ) ) {
+						wc_add_notice( 'We couldn\'t refund you\'re order automatically.','notice' );
+						wp_redirect( $_POST['referring-page']);
+						exit;
+					} elseif ( ! $result ) {
+						return new WP_Error( 'woocommerce_api_create_order_refund_api_failed', __( 'An error occurred while attempting to create the refund using the payment gateway API', 'woocommerce' ), array( 'status' => 500 ) );
+					}
+				}
+
+				// cancel the order if it only contained one item.
+				if ( 1 == count( $items ) ) {
+					$or->update_status('cancelled');	
+				}
+
 				wc_add_notice( 'your booking was successfully cancelled.','success' );
 				wp_redirect( $_POST['referring-page']);
 				exit;
 			} else {
-				wc_add_notice( '(1) we couldn\'t cancel your booking!','notice' );
+				wc_add_notice( '(ERR-1) we couldn\'t cancel your booking!','notice' );
 				wp_redirect( $_POST['referring-page'] );
 				exit;
 			}
 		} else {
 			// internal cancellation error.
-			wc_add_notice( '(2) we couldn\'t cancel your booking!','notice' );
+			wc_add_notice( '(ERR-2) we couldn\'t cancel your booking!','notice' );
 			wp_redirect( $_POST['referring-page'] );
 			exit;
 		}
