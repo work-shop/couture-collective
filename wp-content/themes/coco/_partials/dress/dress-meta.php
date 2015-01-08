7bea31ecda1059a29f5e3e0a72cd5ef7ca1562d2
Greg Nemes <mail@gregnemes.com>
2015-01-08 15:29:49 -0500

    share owned dress styling, and wc-notices styling

--- wp-content/themes/coco/_partials/dress/dress-meta.php
+++ wp-content/themes/coco/_partials/dress/dress-meta.php
@@ -1,9 +1,13 @@
 
 	<div class="bordered-dark-top">
 		<?php
+		
+			$owned = wc_customer_bought_product(
+				$GLOBALS['CC_POST_DATA']['user']->user_email,
+				$GLOBALS['CC_POST_DATA']['user']->ID,
+				$GLOBALS['CC_POST_DATA']['share']->id
+			);
 
-			
-			
 			$id = $GLOBALS['CC_POST_DATA']['id'];
 			$user = $GLOBALS['CC_POST_DATA']['logged_in'];
 	
@@ -17,29 +21,24 @@
 			echo ws_ifdef_do( $title, ws_ifdef_concat('<h6 class="dress-description">',$description,'</h6>') );
 	
 			echo ws_ifdef_do( $user, ws_ifdef_do( $size, ws_ifdef_concat('<p class="h7">SIZE: <span class="numerals h8">',$size,'</span></p>') ) );
-			?>
-
-			<hr class="brand" />
-
-			<?php
-
-			get_template_part('_partials/dress/dress','alpha-pricing');
-
-			?>
-			<?php
-			//<p class="dress-preview h8 m">
-			//	During our preview session, dresses are unavailable for online reservation. Browse the calendar below to see available dates for reservation for this dress, and then email <a href="mailto:info@couturecollective.club">info@couturecollective.club</a> to inquire about a reservation.
-			// </p>
-			?>
+			
+			if( $owned ){ ?>
 	
+				<hr class="brand half" />
+	
+				<p class="h7 uppercase"><span class="icon"><?php get_template_part('_icons/delivery'); ?></span> 
+				In My Closet
+				
+				</p>
+				
+				<p class="remaining-reservations h7 uppercase"> pre-reservations remaining</span>
+				
+				<hr class="brand half" />
+
+			<?php } else{ ?>
+			
+				<hr class="brand" />			
+			
+			<?php }?>
+				
 	</div>
-	
-	<?php if ( $user ) : ?>
-	<div class="col-sm-3-broken">
-		<?php
-	
-		// CALENDAR
-	
-		?>
-	</div>
-	<?php endif; ?>
