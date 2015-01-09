c8268714921cc89996eda5d303e18a402113b1c2
Nic Schumann <nicolas_schumann@brown.edu>
2015-01-09 15:12:40 -0500

    added backup cart; began email formatting

--- wp-content/themes/coco/woocommerce/emails/customer-booking-cancelled.php
+++ wp-content/themes/coco/woocommerce/emails/customer-booking-cancelled.php
@@ -7,37 +7,32 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 ?>
 
+<?php  
+	$dress_id = CC_Controller::get_dress_for_product( $booking->get_product()->id, "rental" );
+	$dress_title = get_field('dress_designer', $dress_id);
+	$dress_description = get_field('dress_description', $dress_id);
+
+
+	$booking_type = CC_Controller::get_booking_type( $booking );
+?>
+
 <?php do_action( 'woocommerce_email_header', $email_heading ); ?>
 
 <?php if ( $booking->get_order() ) : ?>
 	<p><?php printf( __( 'Hello %s', 'woocommerce-bookings' ), $booking->get_order()->billing_first_name ); ?></p>
 <?php endif; ?>
 
-<p><?php _e( 'We are sorry to say that your booking could not be confirmed and has been cancelled. The details of the cancelled booking can be found below.', 'woocommerce-bookings' ); ?></p>
+<p><?php _e( 'Your '.strtolower( cc_booking_noun_string( $booking_type) ).' has been cancelled. The details of the cancelled reservation can be found below.', 'woocommerce-bookings' ); ?></p>
 
 <table cellspacing="0" cellpadding="6" style="width: 100%; border: 1px solid #eee;" border="1" bordercolor="#eee">
 	<tbody>
 		<tr>
-			<th scope="row" style="text-align:left; border: 1px solid #eee;"><?php _e( 'Booked Product', 'woocommerce-bookings' ); ?></th>
-			<td style="text-align:left; border: 1px solid #eee;"><?php echo $booking->get_product()->get_title(); ?></td>
+			<th scope="row" style="text-align:left; border: 1px solid #eee;"><?php _e( cc_booking_noun_string( $booking_type), 'woocommerce-bookings' ); ?></th>
+			<td style="text-align:left; border: 1px solid #eee;"><?php echo $dress_title.' - '.$dress_description; ?></td>
 		</tr>
 		<tr>
-			<th style="text-align:left; border: 1px solid #eee;" scope="row"><?php _e( 'Booking ID', 'woocommerce-bookings' ); ?></th>
-			<td style="text-align:left; border: 1px solid #eee;"><?php echo $booking->get_id(); ?></td>
-		</tr>
-		<?php if ( $booking->has_resources() && ( $resource = $booking->get_resource() ) ) : ?>
-			<tr>
-				<th style="text-align:left; border: 1px solid #eee;" scope="row"><?php _e( 'Booking Type', 'woocommerce-bookings' ); ?></th>
-				<td style="text-align:left; border: 1px solid #eee;"><?php echo $resource->post_title; ?></td>
-			</tr>
-		<?php endif; ?>
-		<tr>
-			<th style="text-align:left; border: 1px solid #eee;" scope="row"><?php _e( 'Booking Start Date', 'woocommerce-bookings' ); ?></th>
-			<td style="text-align:left; border: 1px solid #eee;"><?php echo $booking->get_start_date(); ?></td>
-		</tr>
-		<tr>
-			<th style="text-align:left; border: 1px solid #eee;" scope="row"><?php _e( 'Booking End Date', 'woocommerce-bookings' ); ?></th>
-			<td style="text-align:left; border: 1px solid #eee;"><?php echo $booking->get_end_date(); ?></td>
+			<th style="text-align:left; border: 1px solid #eee;" scope="row"><?php _e( cc_booking_noun_string( $booking_type).' Date', 'woocommerce-bookings' ); ?></th>
+			<td style="text-align:left; border: 1px solid #eee;"><?php echo CC_Controller::get_selected_date( $booking->get_id() ); ?></td>
 		</tr>
 		<?php if ( $booking->has_persons() ) : ?>
 			<?php
@@ -58,5 +53,3 @@ if ( ! defined( 'ABSPATH' ) ) {
 </table>
 
 <p><?php _e( 'Please contact us if you have any questions or concerns.', 'woocommerce-bookings' ); ?></p>
-
-<?php do_action( 'woocommerce_email_footer' ); ?>
