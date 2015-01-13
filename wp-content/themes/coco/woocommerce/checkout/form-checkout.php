30a48b69757541b035074fce171401dce70e3cf4
Greg Nemes <mail@gregnemes.com>
2015-01-13 17:13:31 -0500

    best effort on join checkout confirmation

--- wp-content/themes/coco/woocommerce/checkout/form-checkout.php
+++ wp-content/themes/coco/woocommerce/checkout/form-checkout.php
@@ -46,7 +46,7 @@ $get_checkout_url = apply_filters( 'woocommerce_get_checkout_url', WC()->cart->g
 
 		<?php do_action( 'woocommerce_checkout_after_customer_details' ); ?>
 
-		<h3 id="order_review_heading"><?php _e( 'Your order', 'woocommerce' ); ?></h3>
+		<p class="h7 uppercase" id="order_review_heading"><?php _e( 'Your order: ', 'woocommerce' ); ?></p>
 
 	<?php endif; ?>
 
