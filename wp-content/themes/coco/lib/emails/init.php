8598fb8125c5f96b18effc3ead4f10e3a580059c
Nic Schumann <nicolas_schumann@brown.edu>
2015-01-26 11:20:24 -0500

    refactored email-class loading in emails/init

--- wp-content/themes/coco/lib/emails/init.php
+++ wp-content/themes/coco/lib/emails/init.php
@@ -1,4 +1,5 @@
 <?php
 	require_once('class-cc-dry-cleaning-email.php');
-	require_once('class-cc-cancel-dry-cleaning-email.php');
+	require_once('class-cc-new-user-email.php');
+	//require_once('class-cc-cancel-dry-cleaning-email.php');
 ?>
