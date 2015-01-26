25012960bb4c3c0c049daf9d5e679f028ba69b38
Nic Schumann <nicolas_schumann@brown.edu>
2015-01-26 11:26:34 -0500

    customized activated email templates

--- wp-content/themes/coco/woocommerce/emails/plain/customer-booking-confirmed.php
+++ wp-content/themes/coco/woocommerce/emails/plain/customer-booking-confirmed.php
@@ -14,24 +14,25 @@ if ( $booking->get_order() ) {
 
 echo __(  'Thank you for your reservation!', 'woocommerce-bookings' ) . "\n\n";
 
-//echo "****************************************************\n\n";
+echo "****************************************************\n\n";
 
 echo sprintf( __( 'Your %s of %s %s will be delivered on %s by 5pm.', 'woocommerce-bookings'), 
 		cc_booking_noun_string( $booking->get_resource()->post_title ), 
 		get_field('dress_designer', $dress_id),
 		get_field('dress_description', $dress_id),
-		date( 'F jS, Y', strtotime( CC_Controller::get_selected_date($booking->id) ) );
+		date( 'F jS, Y', strtotime( CC_Controller::get_selected_date($booking->id) ) )
 	) . "\n\n";
-//echo sprintf( __( 'Booking ID: %s', 'woocommerce-bookings'), $booking->get_id() ) . "\n";
 
 echo sprintf( __( 'Please have it ready to pick up on the morning of %s.', 'woocommerce-bookings'), 
 		$booking->get_end_date()
 	) . "\n\n";
 
-echo "If your dress was shipped to you via Fed-Ex, it can be repackaged in the same box it came in, and the box can be affixed with the enclosed label.\n\n";
+echo "If your dress was shipped to you via Fed-Ex, it can be repackaged in the same box it came in, and the box can be affixed with the enclosed label.\n\n";;
 
 echo "Please be aware that failure to have your dress ready for pick up will result in a $100/day late fee.\n\n";
 
 echo "\n****************************************************\n\n";
 
 echo apply_filters( 'woocommerce_email_footer_text', get_option( 'woocommerce_email_footer_text' ) );
+
+?>
--- wp-content/themes/coco/woocommerce/emails/plain/customer-processing-order.php
+++ wp-content/themes/coco/woocommerce/emails/plain/customer-processing-order.php
@@ -10,41 +10,57 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 echo $email_heading . "\n\n";
 
-echo __( "Your order has been received and is now being processed. Your order details are shown below for your reference:", 'woocommerce' ) . "\n\n";
-
 echo "****************************************************\n\n";
 
-do_action( 'woocommerce_email_before_order_table', $order, $sent_to_admin, $plain_text );
+// ---------------------- Define information needed for email.
 
-echo sprintf( __( 'Order number: %s', 'woocommerce'), $order->get_order_number() ) . "\n";
-echo sprintf( __( 'Order date: %s', 'woocommerce'), date_i18n( wc_date_format(), strtotime( $order->order_date ) ) ) . "\n";
+foreach ($order->get_items() as $key => $value) {
+		$product = wc_get_product( ws_fst( $value['item_meta']['_product_id'] ));
+		$product_type = ( is_array($ts = get_the_terms( $product->id, 'product_cat')) ) ? ws_fst( $ts )->name : "";
 
-do_action( 'woocommerce_email_order_meta', $order, $sent_to_admin, $plain_text );
+		if ( $product->product_type == "booking" ) {
+			$dress_id = CC_Controller::get_dress_for_product( $product->id, "rental" );
+			
+			echo __(  'Thank you for your reservation!', 'woocommerce-bookings' ) . "\n\n";
 
-echo "\n" . $order->email_order_items_table( $order->is_download_permitted(), true, $order->has_status( 'processing' ), '', '', true );
+			echo sprintf( __( 'Your reservation of %s - %s will be delivered on %s by 5pm.', 'woocommerce-bookings'),
+					get_field('dress_designer', $dress_id),
+					get_field('dress_description', $dress_id),
+					$value['Booking Date']
+				) . "\n\n";
+			//echo sprintf( __( 'Booking ID: %s', 'woocommerce-bookings'), $booking->get_id() ) . "\n";
 
-echo "----------\n\n";
+			echo sprintf( __( 'Please have it ready to pick up on the following morning.', 'woocommerce-bookings') ) . "\n\n";
 
-if ( $totals = $order->get_order_item_totals() ) {
-	foreach ( $totals as $total ) {
-		echo $total['label'] . "\t " . $total['value'] . "\n";
-	}
+			echo "If your dress was shipped to you via Fed-Ex, it can be repackaged in the same box it came in, and the box can be affixed with the enclosed label.\n\n";
+
+			echo "Please be aware that failure to have your dress ready for pick up will result in a $100/day late fee.\n\n";
+
+		} else if ( $product->product_type == "simple" && $product_type == "share"  ) {
+			$dress_id = CC_Controller::get_dress_for_product( $product->id, "share" );
+
+			echo sprintf( __('Thank you for your purchase of a share in %s - %s. You can manage your dress at %s/closet.'),
+				get_field('dress_designer', $dress_id),
+				get_field('dress_description', $dress_id),
+				home_url()
+			);
+
+		} else {
+
+			$dress_id = CC_Controller::get_dress_for_product( $product->id, "sale" );
+
+			echo sprintf( __('Thank you for your purchase of %s - %s. Your dress will be shipped to you at the end of the season.'),
+				get_field('dress_designer', $dress_id),
+				get_field('dress_description', $dress_id)
+			);
+
+		}
+
+	echo "****************************************************\n\n";
 }
 
-echo "\n****************************************************\n\n";
-
-do_action( 'woocommerce_email_after_order_table', $order, $sent_to_admin, $plain_text );
-
-echo __( 'Your details', 'woocommerce' ) . "\n\n";
-
-if ( $order->billing_email )
-	echo __( 'Email:', 'woocommerce' ); echo $order->billing_email . "\n";
-
-if ( $order->billing_phone )
-	echo __( 'Tel:', 'woocommerce' ); ?> <?php echo $order->billing_phone . "\n";
-
-wc_get_template( 'emails/plain/email-addresses.php', array( 'order' => $order ) );
-
-echo "\n****************************************************\n\n";
+// ---------------------- /
 
 echo apply_filters( 'woocommerce_email_footer_text', get_option( 'woocommerce_email_footer_text' ) );
+
+?>
