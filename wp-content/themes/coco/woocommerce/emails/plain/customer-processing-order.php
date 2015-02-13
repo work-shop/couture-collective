4a2382edf45263352fbd2b38a36a19748c890b39
Nic Schumann <nicolas_schumann@brown.edu>
2015-02-13 12:24:24 -0500

    added additional logic to customer purchase confirmation emails.

--- wp-content/themes/coco/woocommerce/emails/plain/customer-processing-order.php
+++ wp-content/themes/coco/woocommerce/emails/plain/customer-processing-order.php
@@ -8,10 +8,6 @@
  */
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
-echo $email_heading . "\n\n";
-
-echo "****************************************************\n\n";
-
 // ---------------------- Define information needed for email.
 
 foreach ($order->get_items() as $key => $value) {
@@ -19,22 +15,51 @@ foreach ($order->get_items() as $key => $value) {
 		$product_type = ( is_array($ts = get_the_terms( $product->id, 'product_cat')) ) ? ws_fst( $ts )->name : "";
 
 		if ( $product->product_type == "booking" ) {
+			/**
+			 * @todo fix the hard-coded offset interval here...
+			 */
+
 			$dress_id = CC_Controller::get_dress_for_product( $product->id, "rental" );
-			
+
 			echo __(  'Thank you for your reservation!', 'woocommerce-bookings' ) . "\n\n";
 
-			echo sprintf( __( 'Your reservation of %s - %s will be delivered on %s by 5pm.', 'woocommerce-bookings'),
-					get_field('dress_designer', $dress_id),
-					get_field('dress_description', $dress_id),
-					$value['Booking Date']
-				) . "\n\n";
-			//echo sprintf( __( 'Booking ID: %s', 'woocommerce-bookings'), $booking->get_id() ) . "\n";
+			if ( strtotime( $value['Booking Date'] ) > strtotime( '+1 day' )) { 
 
-			echo sprintf( __( 'Please have it ready to pick up on the following morning.', 'woocommerce-bookings') ) . "\n\n";
+				/**
+				 * This is an advance reservation.
+				 */
+				
+				echo sprintf( __( 'Your reservation of %s - %s will be delivered on %s by 5pm.', 'woocommerce-bookings'),
+						get_field('dress_designer', $dress_id),
+						get_field('dress_description', $dress_id),
+						date( "F d Y", strtotime( $value['Booking Date'] . " -1 days" ) )
+					) . "\n\n";
+				
 
-			echo "If your dress was shipped to you via Fed-Ex, it can be repackaged in the same box it came in, and the box can be affixed with the enclosed label.\n\n";
+				echo sprintf( __( 'Please have it ready to pick up on the morning of %s.', 'woocommerce-bookings'), 
+						date( "F d Y", strtotime( $value['Booking Date'] . " +1 days" ) )
+					) . "\n\n";
 
-			echo "Please be aware that failure to have your dress ready for pick up will result in a $100/day late fee.\n\n";
+				
+				echo "If you need to extend this reservation for any reason, please email your request to info@couturecollective.club. Extensions are dependent on availability. A one-week reservation can sometimes be arranged by using two of your pre-reservation dates. Please also be aware that, out of respect for the sartorial rights of all our members, failure to have an item ready for pick up on the designated date will result in a $100/day automatic late fee. Cancellations may be made on the website up to one week prior to the reservation. If you need to cancel a reservation within the week, please email your cancellation request to info@couturecollective.club. Cancellations cannot be made within 48 hours of a reservation. Your item will be delivered and your card will be charged the cleaning/handling fee.\n\n";
+
+			} else {
+				/**
+				 * This is a tomorrow reservation.
+				 */
+
+				echo sprintf( __( 'Your reservation of %s - %s will be delivered on tomorrow.', 'woocommerce-bookings'),
+						get_field('dress_designer', $dress_id),
+						get_field('dress_description', $dress_id)
+					) . "\n\n";
+
+				echo sprintf( __( 'Please have it ready to pick up on the morning of %s. Please also be aware that, out of respect for the sartorial rights of all our members, failure to have an item ready for pick up on the designated date will result in a $100/day automatic late fee. This reservation cannot be cancelled.', 'woocommerce-bookings'), 
+						date( "F d Y", strtotime( $value['Booking Date'] . " +1 days" ) )
+					) . "\n\n";
+
+			}
+			
+ 
 
 		} else if ( $product->product_type == "simple" && $product_type == "share"  ) {
 			$dress_id = CC_Controller::get_dress_for_product( $product->id, "share" );
@@ -49,14 +74,14 @@ foreach ($order->get_items() as $key => $value) {
 
 			$dress_id = CC_Controller::get_dress_for_product( $product->id, "sale" );
 
-			echo sprintf( __('Thank you for your purchase of %s - %s. Your dress will be shipped to you at the end of the season.'),
+			echo sprintf( __('Thank you for your purchase of %s - %s! This item will be delivered to you on April 1st, 2015.'),
 				get_field('dress_designer', $dress_id),
 				get_field('dress_description', $dress_id)
 			);
 
 		}
 
-	echo "****************************************************\n\n";
+	echo "\n\n";
 }
 
 // ---------------------- /
