ac8956a7cd3825b282b0a8f695879115aadb1acb
Nic Schumann <nicolas_schumann@brown.edu>
2015-03-19 15:14:09 -0400

    connected gallery-data to front-end scripts

--- wp-content/themes/coco/_partials/dress/dress-gallery.php
+++ wp-content/themes/coco/_partials/dress/dress-gallery.php
@@ -2,19 +2,35 @@
 	$images = get_field('dress_images', get_the_ID() );
 
 	if ( $images ) {
+
+		//var_dump($images);
+
 ?>
 
 	<div class="row">
-		<div class="col-sm-2 col-xs-2">
+		<?php if (count( $images ) > 1) : ?>
+		<div class="col-sm-2 hidden-xs">
+		<ul class="row">
+			<?php for ( $i = 1; $i < count( $images ); $i++ ) : ?>
+				<li class="small-image co-sm-12 main-image m">
+					<a href="#">
+					<img src-giant="<?php echo $images[ $i ]['sizes']['full']; ?>" src-large="<?php echo $images[ $i ]['sizes']['large']; ?>" src="<?php echo $images[ $i ]['sizes']['thumbnail']; ?>" />
+					</a>
+				</li>
 
-		
+			<?php endfor; ?>
+		</ul>
 		</div>
+	<?php endif; ?>
 
-		<div class="col-sm-10 col-xs-8 main-image off">
+		<div id="main-image" class="col-sm-10 col-xs-12 main-image off">
 			<?php 
-			if ( has_post_thumbnail() ) {
-				the_post_thumbnail('dress-large');
-			} else {
+			if ( has_post_thumbnail() ) { ?>
+
+				<div src-giant="<?php echo $images[ 0 ]['sizes']['full']; ?>" class="magnifier"></div>
+				<img src-giant="<?php echo $images[ $i ]['sizes']['full']; ?>" src-small="<?php echo $images[ 0 ]['sizes']['thumbnail']; ?>" src="<?php echo $images[ 0 ]['sizes']['large']; ?>" />
+				
+			<?php } else {
 				echo '<img src="' . get_bloginfo( 'template_directory' ) . '/_/img/thumbnail-default.jpg" />';
 			}
 			?>	
