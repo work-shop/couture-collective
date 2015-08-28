f739a0ce8fd0d235680413116218186cefb191ad
Nic Schumann <nicolas_schumann@brown.edu>
2015-08-28 11:46:17 -0400

    added a 'blank' sneak peak template

---
+++ themes/coco/page-sneak-peak.php
@@ -0,0 +1,15 @@
+<?php get_header();?>
+
+<div id="contact" class="template template-page">	
+	
+	<section id="contact-introduction" class="page-introduction block m2">	
+		
+	</section>
+	
+	<section id="dress-masonry" class="block m3">	
+				
+	</section>	
+	
+</div>	
+
+<?php get_footer(); ?>
