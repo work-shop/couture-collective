dfde873bd8f32c5ab181cc4a647014f8cee8c084
Nic Schumann <nicolas_schumann@brown.edu>
2015-09-01 11:40:34 -0400

    added a sneak peak page with scaffolding

--- themes/coco/page-sneak-peak.php
+++ themes/coco/page-sneak-peak.php
@@ -1,15 +1,39 @@
+<?php if ( get_field('sneak_peak_active', 'option') ) { ?>
+
+<?php
+
+$sneak_peak_dresses = get_field('sneak_peak_images', 'option');
+
+?>
+
 <?php get_header();?>
 
 <div id="contact" class="template template-page">	
-	
-	<section id="contact-introduction" class="page-introduction block m2">	
 		
+	<hr class="page-header-rule"/>	
+
+	<section id="sneak-peak-introduction" class="page-introduction block m2">	
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12 centered">
+					<?php echo get_field('sneak_peak_description', 'option'); ?>
+				</div>
+			</div>
+		</div>
 	</section>
 	
 	<section id="dress-masonry" class="block m3">	
-				
+		<div class="container">
+			<div class="row">
+				<div id="dress-masonry-container" class="col-sm-12"></div>
+			</div>	
+		</div>
 	</section>	
+
+	<script type="text/javascript"></script>
 	
 </div>	
 
 <?php get_footer(); ?>
+
+<?php } else { get_template_part('_partials/placeholder/placeholder', 'forward'); } ?>
