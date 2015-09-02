c7b0606137b0c4f2179c75dcaeb76ff124ed0839
Nic Schumann <nicolas_schumann@brown.edu>
2015-09-02 13:32:14 -0400

    fixed images sizes on half-width images

--- themes/coco/_partials/sneakpeak/sneakpeak-card.php
+++ themes/coco/_partials/sneakpeak/sneakpeak-card.php
@@ -6,6 +6,6 @@ $dress = $GLOBALS['dress'];
 
 <div class="m1 p-5 <?php echo $dress['image_width']; ?>">
 	<div class="product-image">
-	<img class="shadow" src="<?php echo $dress['image']['url']?>">
+	<img class="shadow" src="<?php echo $dress['image']['sizes']['large']; ?>">
 	</div>
 </div>
