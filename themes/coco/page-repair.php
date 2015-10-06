5c82f1e3c14c247f0a365dbd9b8c43bcfc527227
Nic Schumann <nicolas_schumann@brown.edu>
2015-10-06 09:16:25 -0400

    modified testing script

--- themes/coco/page-repair.php
+++ themes/coco/page-repair.php
@@ -10,9 +10,9 @@ if ( get_current_user_id() == 1) {
 
 	global $wpdb;
 
-	$sql1 = "SELECT post_title, COUNT(*) c FROM {$wpdb->prefix}posts WHERE post_type = %s AND post_title LIKE %s GROUP BY post_title HAVING c > 1";
+	$sql1 = "SELECT post_title, COUNT(*) c FROM {$wpdb->prefix}posts WHERE post_type = %s AND ((post_title LIKE %s) OR (post_title LIKE %s) OR (post_title LIKE %s)) GROUP BY post_title HAVING c > 1";
 
-	$column1 = $wpdb->get_col( $wpdb->prepare( $sql1, 'product', '%Share%' ) );
+	$column1 = $wpdb->get_col( $wpdb->prepare( $sql1, 'product', '%Share%', '%Rental%', '%Sale%') );
 
 	$column1 = '("' . join( '","', $column1 ) . '")';
 
@@ -43,11 +43,37 @@ if ( get_current_user_id() == 1) {
 		$post = $posts[ $ID ];
 
 		if ( $post->ID != $originals[ $post->post_title ] ) {
-			echo "deleting post $post->ID: $post->post_title";
+			echo "<b>post $post->ID: $post->post_title</b><br/>";
 
-			wp_delete_post( $post->ID, true );
+			$type_guess = explode( ' ', $post->post_title);
+			$type_guess = $type_guess[ count( $type_guess ) - 1 ];
+			$type_guess = strtolower( $type_guess );
 
-			echo " ...deleted. <br/>";
+			$dress_id = CC_Controller::get_dress_for_product( $post->ID, $type_guess );
+
+			if ( $dress_id ) {
+				echo "Linked Dress: $dress_id<br/>";
+
+				$returned_id = get_field( CC_Controller::$field_keys[ $type_guess . '_product' ], $dress_id )[0]->ID;
+
+				echo "Retrieved $type_guess Product: $returned_id -- Selected $type_guess Product: $post->ID<br/>";
+
+				echo "Equal: " . ($returned_id == $post->ID) . "<br/>";
+
+			} else {
+
+				echo "No Linked Dress... <br/>";
+
+			}
+
+			echo "<br/>";
+
+			
+
+
+
+			//wp_delete_post( $post->ID, true );
+			//echo " ...deleted. <br/>";
 		} 
 
 	}
